185a3a9801962b97e0921dba19e08a8a68530904
mhuskey <matthewhuskey@me.com>
2018-09-01 00:14:40 -0400

    Sign Up Form
    • Added and configured a form for signing up a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+  
   def new
+    @user = User.new
   end
+  
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      flash[:success] = "Account created!"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  
+  private
+    
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+    
 end
