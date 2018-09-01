8ad323246e94eb7467f4e0a54366c00d84456233
Matthew Huskey <matthewhuskey@me.com>
2018-09-01 19:52:03 -0400

    Edit & Update User
    • Configured the form for editing an existing user
    • The project is partially using a Dracula color theme, at least for now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,17 @@ class UsersController < ApplicationController
   end
   
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated!"
+      redirect_to root_path
+    else
+      render 'edit'
+    end
   end
   
   
