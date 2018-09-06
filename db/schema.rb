6538a27d4aa4ed99adcb68dcafde91e6ca2dd662
mhuskey <matthewhuskey@me.com>
2018-09-05 23:43:20 -0400

    Remember & Forget User
    • Users can choose to be remembered via a checkbox
    • Logging out will forget the user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_02_020003) do
+ActiveRecord::Schema.define(version: 2018_09_05_171716) do
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_09_02_020003) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
